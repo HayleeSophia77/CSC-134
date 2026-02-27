1e971c31589aceb5129b1fa6a5a5c294990bce21
VerryBerryBear <paredes.s.lee@gmail.com>
2026-02-27 17:53:41 +0000

    Finished Q3 for real now

--- Module 2/M2HW/M2HW1_Paredes_Q3.cpp
+++ Module 2/M2HW/M2HW1_Paredes_Q3.cpp
@@ -5,3 +5,31 @@ Haylee Paredes
 02/27/2026
 */
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Ask how many pizzas ordered, how many slices per pizza, and how many visitors
+    // Variables
+    int pizzas;
+    int slicePerPizza;
+    int visitors;
+
+    cout << "How many pizzas were ordered? ";
+    cin >> pizzas;
+
+    cout << "How many slices per pizza were there? ";
+    cin >> slicePerPizza;
+
+    cout << "How many visitors attended the party? ";
+    cin >> visitors;
+
+    // Calculate
+    int totalSlices = pizzas * slicePerPizza;
+    int eaten = visitors * 3;
+    int leftover = totalSlices - eaten;
+
+    cout << "Leftover slices of pizza: " << leftover << endl;
+
+    return 0;
+}
