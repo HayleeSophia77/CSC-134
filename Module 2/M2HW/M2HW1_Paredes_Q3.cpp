98b4e1faea08e89f5df11c9f1fe95b0f1e495bdd
VerryBerryBear <paredes.s.lee@gmail.com>
2026-02-27 17:32:11 +0000

    Finished Q3

--- Module 2/M2HW/M2HW1_Paredes_Q3.cpp
+++ Module 2/M2HW/M2HW1_Paredes_Q3.cpp
@@ -5,8 +5,19 @@ Haylee Paredes
 02/25/2026
 */
 
-// Use string school, & string team
-string school = "FTCC";
-string team = "Trojans";
+# include <iostream>
+using namespace std;
 
-cout << "Let's Go " << school << endl;
+int main() {
+    // Use string school, & string team
+    string school = "FTCC";
+    string team = "Trojans";
+
+    // Wasn't required but I wanted to add a explantion point to make it look more fun and cheerful.
+    cout << "Let's Go " << school << "!" << endl;
+    cout << "Let's Go " << school << "!" << endl;
+    cout << "Let's Go " << school << "!" << endl;
+    cout << "Let's Go " << team << "!" << endl;
+
+    return 0;
+}
