b1b2fd08f0c99aa72c8fdba5db65539c8047f7ef
VerryBerryBear <paredes.s.lee@gmail.com>
2026-02-28 18:37:20 +0000

    Finiahed Q1

--- Module 2/M2HW/M2HW1_Paredes_Q1.cpp
+++ Module 2/M2HW/M2HW1_Paredes_Q1.cpp
@@ -5,3 +5,46 @@ Haylee Paredes
 02/28/2026
 */
 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Variables
+    string firstName;
+    string lastName;
+    double balance;
+    double deposit;
+    double withdraw;
+
+
+    cout << "Welcome to Question One Banking Transactions!";
+
+    // Get user name
+    cout << "\nEnter your first name: ";
+    cin >> firstName;
+    cout << "Enter your last name: ";
+    cin >> lastName;
+    cout << "Name on account: " << firstName << " " << lastName << endl;
+
+    // Get amounts for user account
+    cout << "\nStarting balance: ";
+    cin >> balance;
+    cout << "Deposit amount: ";
+    cin >> deposit;
+    cout << "Withdrawal amount: ";
+    cin >> withdraw;
+
+    // Calculate
+    double finalBal = balance + deposit - withdraw;
+    cout << fixed << setprecision(2);
+
+    // Display info.
+    cout << "\n";
+    cout << "Account Information";
+    cout << "\nAccount Owner: " << firstName << " " << lastName << endl;
+    cout << "Account #: 2963418" << endl;
+    cout << "Final balance: $" << finalBal << endl;
+
+    return 0;
+}
