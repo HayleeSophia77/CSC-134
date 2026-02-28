664cfdf8db473a019becf220ad027420de747076
VerryBerryBear <paredes.s.lee@gmail.com>
2026-02-28 22:12:02 +0000

    Finished M3T1

---
+++ Module 3/M3T1_ParedesHaylee.cpp
@@ -0,0 +1,36 @@
+// CSC-134
+// M3T1
+// Haylee Paredes
+// 2/28/2026
+
+// Get width and length of rectangle from user and find the area.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Variables
+    double len1, wid1, len2, wid2;
+    double area1, area2;
+
+    // Ask user for length and width
+    cout << "What's the length of the first rectangle? ";
+    cin >> len1;
+    cout << "What's the width of the first rectangle? ";
+    cin >> wid1;
+    cout << "What's the length of the second rectangle? ";
+    cin >> len2;
+    cout << "What's the width of the second rectangle? ";
+    cin >> wid2;
+
+    // Find the area
+
+    area1 = len1 * wid1;
+    area2 = len2 * wid2;
+
+    // Display the area
+    cout << "Area of rectangle 1: "<< area1 << endl;
+    cout << "Area of rectangle 2: "<< area2 << endl;
+
+    return 0;
+}
