8a1f7b2c21936c977a17da1765cf39e951c6ff9c
VerryBerryBear <paredes.s.lee@gmail.com>
2026-03-01 21:14:05 +0000

    Finished M3T2

--- Module 3/M3T2/M3T2_ParedesHaylee.cpp
+++ Module 3/M3T2/M3T2_ParedesHaylee.cpp
@@ -28,18 +28,45 @@ int main() {
     const int MAX = 6; 
     // Note: Divide by MAX, & keep the remainder
     // int roll = rand() % MAX;
-    int roll;
-    roll = (rand() % MAX) + 1;
+    // int roll;
+    int roll_1, roll_2, total;
+    // roll = (rand() % MAX) + 1;
+    roll_1 = (rand() % MAX) + 1;
 
-    cout << "You rolled a " << roll << "!" << endl;
+    cout << "You rolled a " << roll_1 << "!" << endl;
     // cout << "and a " << rand() % MAX << "!" << endl;
     // cout << "and a " << rand() % MAX << "!" << endl;
 
-    roll = (rand() % MAX) + 1;
-    cout << "You rolled a " << roll << "!" << endl;
+    // roll = (rand() % MAX) + 1;
+    roll_2 = (rand() % MAX) + 1;
+    cout << "You rolled a " << roll_2 << "!" << endl;
 
-    roll = (rand() % MAX) + 1;
-    cout << "You rolled a " << roll << "!" << endl;
+    // roll = (rand() % MAX) + 1;
+    // cout << "You rolled a " << roll << "!" << endl;
+
+    total = roll_1 + roll_2;
+    cout << "Your total roll is: " << total << endl;
+
+    // Note: Playing craps
+    // Note: 7 & 11 = win, 2 & 12 = lose, anything else = comes later
+    if (total == 7) {
+        cout << "Lucky seven! You win!" << endl;
+    }
+    else if (total == 11) {
+        cout << "Eleven is a winner!" << endl;
+    }
+    else if (total == 2) {
+        cout << "Snake eyes! Too bad, you lose!" << endl;
+    }
+    else if (total == 3) {
+        cout << "Sorry, three is unucky, you lose!" << endl;
+    }
+    else if (total == 12) {
+        cout << "Boxcars! Sorry, you lost!" << endl;
+    }
+    else {
+        cout << "Your point is: " << total << " but we'll do that part later!" << endl;
+    }
 
     return 0;
 }
