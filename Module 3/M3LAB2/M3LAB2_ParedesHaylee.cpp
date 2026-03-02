834deb4e7394faeb021ade1a36685cbe2921cc8f
VerryBerryBear <paredes.s.lee@gmail.com>
2026-03-02 21:37:08 +0000

    testing testing

--- Module 3/M3LAB2/M3LAB2_ParedesHaylee.cpp
+++ Module 3/M3LAB2/M3LAB2_ParedesHaylee.cpp
@@ -7,10 +7,38 @@
 using namespace std;
 
 int main() {
+    // Welcome message
     cout << "Welcome to Number to Letter Grade Converter!" << endl;
 
     // Variables
     int numGrade;
 
+    // Ask user for # grade
+    cout << "\nPlease enter a numeric grade from 0 to 100: " << endl;
+    cin >> numGrade;
+
+    // Convert # grade to letter grade
+    // Note: I am very particular about the formatting of the output, so I added spaces and new lines cuz I find it looks cleaner and more readable.
+    if (numGrade >= 90 && numGrade <= 100) {
+        cout << "\nYour grade is an A." << endl;
+    }
+    else if (numGrade >= 80 && numGrade < 89) {
+        cout << "\nYour grade is a B." << endl;
+    }
+    else if (numGrade >= 70 && numGrade < 79) {
+        cout << "\nYour grade is a C." << endl;
+    }
+    else if (numGrade >= 60 && numGrade < 69) {
+        cout << "\nYour grade is a D." << endl;
+    }
+    else if (numGrade >= 0 && numGrade < 59) {
+        cout << "\nYour grade is an F." << endl;
+    }
+    else {
+        cout << "\nYou've entered an invalid grade." << endl;
+    }
+
+    // Thank user & end program
+    cout << "\nThank you for using the Number to Letter Grade Converter! Goodbye!" << endl;
     return 0;
 }
