4fe03530a3e377a655618a677b954951442c450f
VerryBerryBear <paredes.s.lee@gmail.com>
2026-03-02 21:21:17 +0000

    Started M3LAB2

---
+++ Module 3/M3LAB2/M3LAB2_ParedesHaylee.cpp
@@ -0,0 +1,16 @@
+// CSC-134
+// M3LAB2
+// 03/02/2026
+// Haylee Paredes
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to Number to Letter Grade Converter!" << endl;
+
+    // Variables
+    int numGrade;
+
+    return 0;
+}
