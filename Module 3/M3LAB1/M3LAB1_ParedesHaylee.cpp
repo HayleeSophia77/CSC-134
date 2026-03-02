15eaf12cdfedd98caa347ab182dc2fd37d95b382
VerryBerryBear <paredes.s.lee@gmail.com>
2026-03-02 14:53:18 +0000

    Started M3LAB1 - Example 1 & 2

---
+++ Module 3/M3LAB1/M3LAB1_ParedesHaylee.cpp
@@ -0,0 +1,39 @@
+// CSC-134
+// M3LAB1
+// 03/02/2026
+// Haylee Paredes
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Example 1: simple program.
+    cout << "Hello World!" << endl;
+
+    // Example 2: Ask user a question and respond to it. Should be able to run it and test it with different inputs.
+    // NOTE: example test values: 1, 2, 3, banana
+
+    // NOTE: Declare variable
+    int choice;
+
+    // NOTE: Ask user a question
+    cout << "Do you choose door 1 or door 2?" << endl;
+    cout << "Type 1 or 2: " << endl;
+    cin >> choice;
+
+    // NOTE: if statement = decision based on user input
+    if (1 == choice) {
+        cout << "You chose door 1." << endl;
+    }
+    else if (2 == choice) {
+        cout << "You chose door 2." << endl;
+    }
+    else {
+        cout << "I'm sorry, that is not a valid choice. Please choose door 1 or door 2." << endl;
+    }
+
+    // NOTE: end of program
+    // NOTE: runs no matter what they choose
+    cout << "Thank you for playing!" << endl;
+    return 0;
+} // NOTE: end of main() method
