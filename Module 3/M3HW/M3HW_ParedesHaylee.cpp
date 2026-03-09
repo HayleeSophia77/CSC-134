6090b2bd0765db9784220a361b2fa015c87c5712
VerryBerryBear <paredes.s.lee@gmail.com>
2026-03-09 18:55:58 +0000

    started M3HW

---
+++ Module 3/M3HW/M3HW_ParedesHaylee.cpp
@@ -0,0 +1,18 @@
+// CSC 134
+// M3HW1 - Gold
+// Haylee Paredes
+// 03/09/2026
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// Declare if more functions
+void quest1();
+// void quest2();
+// void quest3();
+// void quest4();
+
+int main() {
+    
+}
