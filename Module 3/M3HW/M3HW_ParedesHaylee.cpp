93f5ec6cf785e1337d8f71f5881f54cb230c4d4e
VerryBerryBear <paredes.s.lee@gmail.com>
2026-03-09 20:09:27 +0000

    Finished Q1, Started Q2

--- Module 3/M3HW/M3HW_ParedesHaylee.cpp
+++ Module 3/M3HW/M3HW_ParedesHaylee.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <iomanip>
+#include <string>
 using namespace std;
 
 // Declare if more functions
@@ -19,7 +20,8 @@ int main() {
     bool keep_going = true;
     
     while (true == keep_going) {
-        cout << "M3HW - Haylee Paredes" << endl;
+        cout << "\nM3HW - Haylee Paredes" << endl;
+        cout << "-------------------------" << endl;
         cout << "Let's Go For GOLD!" << endl;
         cout << "1. Question 1 - Chatbot" << endl;
         //cout << "2. Question 2 - Receipt Calculator" << endl;
@@ -27,33 +29,34 @@ int main() {
         //cout << "4. Question 4 - Math Practice" << endl;
         cout << "0. Exit" << endl;
 
+        cout << "\nPlease choose a question number from the menu (0-4): ";
         cin >> choice;
         if (1 == choice) {
             quest1();
         }
         else if (0 == choice) {
-            cout << "Bye! Have a great day!" << endl;
+            cout << "\nBye! Have a great day!" << endl;
             // Exit program
             keep_going = false;
         }
         else {
-            cout << "Not a valid choice. Please choose a number from the menu." << endl;
+            cout << "\nNot a valid choice. Please choose a number from the menu." << endl;
         }
     }
     return 0;
 }
 
-quest1() {
-    cout << "1. Question 1 - Chatbot" << endl;
+void quest1() {
+    cout << "\n1. Question 1 - Chatbot" << endl;
     string chatInput;
     cout << "Hello, I’m a C++ program!" << endl;
     cout << "Do you like me? Please type yes or no." << endl;
     cin >> chatInput;
 
-    if (yes == chatInput) {
+    if ("yes" == chatInput) {
         cout << "That’s great! I’m sure we’ll get along." << endl;
     }
-    else if (no == chatInput) {
+    else if ("no" == chatInput) {
         cout << "Well, maybe you’ll learn to like me later." << endl;
     }
     else {
