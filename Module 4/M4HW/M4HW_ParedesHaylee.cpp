2411527a4cb71f542bacb37f71909b71ddd85f83
VerryBerryBear <paredes.s.lee@gmail.com>
2026-03-23 23:02:19 +0000

    Finished M4HW

--- Module 4/M4HW/M4HW_ParedesHaylee.cpp
+++ Module 4/M4HW/M4HW_ParedesHaylee.cpp
@@ -12,7 +12,13 @@ int main() {
     cout << "Enter a number from 1 to 12: ";
     cin >> num;
 
-    // Count starts at 1 and ends at 12
+    // Keep asking for # until valid
+    while (num < 1 || num > 12) {
+        cout << "Invalid input! \nPlease enter a number from 1 to 12: ";
+        cin >> num;
+    }
+
+    // Count starts at 1 & ends at 12
     int n = 1;
     while (n <= 12) {
         cout << num << " times " << n << " is " << num * n << "." << endl;
