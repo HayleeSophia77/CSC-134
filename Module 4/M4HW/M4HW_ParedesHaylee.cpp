cece88f14e4ca8874bcf70dca42f4387b420cc58
VerryBerryBear <paredes.s.lee@gmail.com>
2026-03-23 22:57:36 +0000

    Silver complete

--- Module 4/M4HW/M4HW_ParedesHaylee.cpp
+++ Module 4/M4HW/M4HW_ParedesHaylee.cpp
@@ -7,9 +7,16 @@
 using namespace std;
 
 int main() {
+    // Get # from user
+    int num;
+    cout << "Enter a number from 1 to 12: ";
+    cin >> num;
+
+    // Count starts at 1 and ends at 12
     int n = 1;
     while (n <= 12) {
-        cout << 5 << " times " << n << " is " << 5 * n << "." << endl;
+        cout << num << " times " << n << " is " << num * n << "." << endl;
+        // Add 1 to n each time so it counts up to 12
         n++;
     }
     return 0;
